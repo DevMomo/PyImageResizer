b41208e2a11e802a2f8ceea77317d1d255c81190
Monir Imamverdi <monir.imamverdi@gmail.com>
2019-08-20 17:13:22 -0600

    Sys arg handling

--- resize.py
+++ resize.py
@@ -1,8 +1,21 @@
 import sys
+import os
 from PIL import Image 
 
-if (sys.argv):
-	im = Image.open(sys.argv[1])
-	im.show()
+"""
+This script will resize one or more images to a set percentage using the Python Imaging Library
+"""
+
+if __name__ == "__main__":
+	try:
+		images = sys.argv[1:-1]
+		percentage = float(sys.argv[-1])
+	except IndexError:
+		print ("Usage: " + os.path.basename(__file__) + " <image files> <percentage>")	
+		sys.exit(1)
+	except ValueError:
+		print ("Usage: " + os.path.basename(__file__) + " <image files> <percentage>")
+		sys.exit(1)
+
 
 
