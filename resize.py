b860f3bccdb0b92e5ab06af1805f8ea7d24acf1a
Monir Imamverdi <monir.imamverdi@gmail.com>
2019-08-21 12:32:56 -0600

    Batch image resize complete

--- resize.py
+++ resize.py
@@ -1,9 +1,9 @@
-import sys
-import os
+import os, sys
 from PIL import Image 
 
 """
-This script will resize one or more images to a set percentage using the Python Imaging Library
+This script will resize one or more images to a set percentage using the Python Imaging Library.
+Output images are converted to JPG.
 """
 
 if __name__ == "__main__":
@@ -17,5 +17,25 @@ if __name__ == "__main__":
 		print ("Usage: " + os.path.basename(__file__) + " <image files> <percentage>")
 		sys.exit(1)
 
+
+
+	for image in images:
+
+		output = os.path.splitext(image)[0] + "_RESIZED" + str(percentage) + ".jpg" # output file name
+		
+		if image != output:
+			try:
+				im = Image.open(image)
+
+				# Calculate new size of resized image
+				maxSize = max(im.height, im.width)
+				maxSize = maxSize * percentage # shrink by given percentage
+				size = (maxSize, maxSize)
+
+				im.thumbnail(size, Image.BICUBIC)
+				im.save(output, "JPEG")
+
+			except IOError:
+				print ("File error for %s" % image)
 
 
